bbfcb25fb99d0121e276b9a70b45467471cecb7a
zxy <84235789@qq.com>
2020-11-23 19:29:26 +0800

    .

--- src/main/java/com/cupshe/ak/common/AutoTraceIdConfig.java
+++ src/main/java/com/cupshe/ak/common/AutoTraceIdConfig.java
@@ -1,24 +1,25 @@
 package com.cupshe.ak.common;
 
+import com.cupshe.ak.core.Kv;
 import com.cupshe.ak.text.StringUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
-import static com.cupshe.ak.common.BaseConstant.TRACE_ID_KEY;
-import static com.cupshe.ak.common.BaseConstant.TRACE_ID_STORE;
+import static com.cupshe.ak.common.BaseConstant.*;
 
 /**
  * TraceIdInterceptor
  *
  * @author zxy
  */
-@Configuration
 @Slf4j
 public class AutoTraceIdConfig implements WebMvcConfigurer {
 
@@ -28,15 +29,41 @@ public class AutoTraceIdConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**");
     }
 
-    public class TraceIdInterceptor implements HandlerInterceptor {
+    public static class TraceIdInterceptor implements HandlerInterceptor {
+
         @Override
         public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) {
+            storeRequestHeaders(req, new ArrayList<>());
+            storeRequestParams(req, new ArrayList<>());
+            storeTraceId(req);
+            return true;
+        }
+
+        private void storeRequestHeaders(HttpServletRequest req, List<Kv> headers) {
+            Enumeration<String> headerNames = req.getHeaderNames();
+            for (String key; headerNames.hasMoreElements(); ) {
+                key = headerNames.nextElement();
+                headers.add(new Kv(key, req.getHeader(key)));
+            }
+
+            REQ_HEADERS_STORE.set(headers);
+        }
+
+        private void storeRequestParams(HttpServletRequest req, List<Kv> params) {
+            Enumeration<String> paramNames = req.getParameterNames();
+            for (String key; paramNames.hasMoreElements(); ) {
+                key = paramNames.nextElement();
+                params.add(new Kv(key, req.getParameter(key)));
+            }
+
+            REQ_PARAMS_STORE.set(params);
+        }
+
+        private void storeTraceId(HttpServletRequest req) {
             String traceId = req.getHeader(TRACE_ID_KEY);
             if (StringUtils.isNotBlank(traceId)) {
                 TRACE_ID_STORE.set(traceId);
             }
-
-            return true;
         }
     }
 }
--- src/main/java/com/cupshe/ak/common/BaseConstant.java
+++ src/main/java/com/cupshe/ak/common/BaseConstant.java
@@ -1,5 +1,9 @@
 package com.cupshe.ak.common;
 
+import com.cupshe.ak.core.Kv;
+
+import java.util.List;
+
 /**
  * BaseConstant
  *
@@ -10,6 +14,12 @@ public class BaseConstant {
     /*** trace-id store */
     public static final InheritableThreadLocal<String> TRACE_ID_STORE = new InheritableThreadLocal<>();
 
+    /*** request headers store */
+    public static final InheritableThreadLocal<List<Kv>> REQ_HEADERS_STORE = new InheritableThreadLocal<>();
+
+    /*** request params store */
+    public static final InheritableThreadLocal<List<Kv>> REQ_PARAMS_STORE = new InheritableThreadLocal<>();
+
     /*** trace-id key */
     public static final String TRACE_ID_KEY = "Trace-ID";
 
@@ -18,4 +28,7 @@ public class BaseConstant {
 
     /*** 身份认证字段 */
     public static final String AUTHORIZATION = "Authorization";
+
+    /*** 身份认证字段 */
+    public static final String TOKEN_KEY = "token";
 }
